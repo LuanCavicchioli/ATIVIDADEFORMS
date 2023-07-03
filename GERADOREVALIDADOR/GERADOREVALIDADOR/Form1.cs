333a43201651cee7a05ae89a026f383c6f020160
LuanCavicchioli <134647816+LuanCavicchioli@users.noreply.github.com>
2023-07-03 11:45:17 -0300

    Forms de validador e gerador de cpf e cnpj

---
+++ GERADOREVALIDADOR/GERADOREVALIDADOR/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GERADOREVALIDADOR
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            form2.FormClosed += Form2_FormClosed; // o
+            form2.Show();
+            Hide(); 
+        }
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Show();
+        }
+
+        private void SAIR_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (TxtComandoGerado.Text == "" || RBNCpf.Text == "" || RBNCnpj.Text == "")
+            {
+                MessageBox.Show("Por favor, escolha uma das duas opções!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (RBNCpf.Checked)
+            {
+                string cpf = TxtComandoGerado.Text.Replace(".", "").Replace("-", "");
+
+                if (cpf.Length != 11)
+                {
+                    MessageBox.Show("O CPF deve conter 11 dígitos.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (!ValidateInputCPF(cpf))
+                {
+                    MessageBox.Show("O CPF deve conter apenas números.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (ValidarCPF(cpf))
+                {
+                    MessageBox.Show("CPF VÁLIDO!", "CORRETO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("CPF INVÁLIDO!", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (RBNCnpj.Checked)
+            {
+                string cnpj = TxtComandoGerado.Text.Replace(".", "").Replace("-", "").Replace("/", "");
+
+
+
+                if (cnpj.Length != 14)
+                {
+                    MessageBox.Show("O CNPJ deve conter 14 dígitos.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (!ValidateCNPJInput(cnpj))
+                {
+                    MessageBox.Show("O CNPJ deve conter apenas números.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (ValidarCNPJ(cnpj))
+                {
+                    MessageBox.Show("CNPJ VÁLIDO!", "CORRETO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("CNPJ INVÁLIDO!", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void TxtComandoGerado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gERADORToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gERADORToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Form2 Gerador = new Form2();
+            Gerador.ShowDialog();
+        }
+        private bool ValidarCPF(string cpf)
+        {
+
+
+
+            int[] mult = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] mult1 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int dig1 = 0;
+            int dig2 = 0;
+            int[] x = new int[11];
+            int soma = 0, soma1 = 0;
+
+
+
+
+
+
+            for (int i = 0; i < 11; i++)
+            {
+                x[i] = int.Parse(cpf[i].ToString());
+            }
+
+
+
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma += x[i] * mult[i];
+                dig1 = (soma * 10) % 11;
+                if (dig1 == 10 || dig1 == 11)
+                {
+                    dig1 = 0;
+                }
+            }
+
+
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma1 += x[i] * mult1[i];
+                dig2 = (soma1 * 10) % 11;
+                if (dig2 == 10 || dig2 == 11)
+                {
+                    dig2 = 0;
+                }
+            }
+            if (dig1 == x[9] && dig2 == x[10])
+                return true;
+
+
+
+            else return false;
+        }
+        private bool ValidarCNPJ(string cnpj)
+        {
+            if (!ValidateCNPJInput(cnpj))
+            {
+                MessageBox.Show("O CPNJ DEVE CONTER APENAS NUMEROS ", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            int[] mult = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] mult2 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int dig1 = 0;
+            int dig2 = 0;
+            int[] x = new int[14];
+            int soma = 0, soma1 = 0;
+
+
+
+            for (int i = 0; i < 14; i++)
+            {
+                x[i] = int.Parse(cnpj[i].ToString());
+            }
+
+
+
+            for (int i = 0; i < 12; i++)
+            {
+                soma += x[i] * mult[i];
+            }
+            dig1 = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+
+
+
+            for (int i = 0; i < 13; i++)
+            {
+                soma1 += x[i] * mult2[i];
+            }
+            dig2 = (soma1 % 11 < 2) ? 0 : -11 - (soma1 % 11);
+            if (dig1 == x[12] && dig2 == x[13])
+                return true;
+
+
+
+            else return false;
+
+
+
+
+        }
+        static bool ValidateInputCPF(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool ValidateCNPJInput(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void RBNCpf_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
